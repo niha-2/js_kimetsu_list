dd3b579ab4506fe788f60114b5a3e11ad7e7e216
niha-2 <gen.3polym.hn@gmail.com>
2024-07-16 20:36:45 +0900

    [fix] レビュー指摘対応：共通処理を関数化

--- src/App.js
+++ src/App.js
@@ -8,8 +8,7 @@ export class App {
     const containerElement = document.querySelector("#chara-list");
     const charaFilterElements = document.querySelectorAll('input[name="filter"]');
 
-    let currentFilter = 'all';
-    let fetchUrl = "https://ihatov08.github.io/kimetsu_api/api/all.json";
+    fetchAndRenderCharaInfo('all');
 
     charaFilterElements.forEach(emt => {
       emt.addEventListener("change", (event) => {
@@ -17,42 +16,18 @@ export class App {
         const loadingElement = element`<img src="../img/loading.jpeg">`;
         render(loadingElement, containerElement);
 
-        currentFilter = event.target.value;
-        switch (currentFilter) {
-          case 'kisatsutai':
-            fetchUrl = "https://ihatov08.github.io/kimetsu_api/api/kisatsutai.json";
-            break;
-          case 'hashira':
-            fetchUrl = "https://ihatov08.github.io/kimetsu_api/api/hashira.json";
-            break;
-          case 'oni':
-            fetchUrl = "https://ihatov08.github.io/kimetsu_api/api/oni.json";
-            break;
-          default:
-            fetchUrl = "https://ihatov08.github.io/kimetsu_api/api/all.json";
-            break;
-        }
-
-        fetchCharaInfo().then(function (json) {
-          const charaListView = new CharaListView();
-          const charaListElement = charaListView.createElement(json);
-          render(charaListElement, containerElement);
-        });
-
+        fetchAndRenderCharaInfo(event.target.value);
       });
     });
 
-    async function fetchCharaInfo() {
+    async function fetchAndRenderCharaInfo(currentFilter) {
+      const fetchUrl = `https://ihatov08.github.io/kimetsu_api/api/${currentFilter}.json`;
       const response = await fetch(fetchUrl);
       const json = await response.json();
-      return json;
-    }
-
-    fetchCharaInfo().then(function (json) {
       const charaListView = new CharaListView();
       const charaListElement = charaListView.createElement(json);
       render(charaListElement, containerElement);
-    });
+    }
   }
 
 }
